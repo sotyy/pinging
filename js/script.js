358e9abb9dc5bb38b58aa8ec9ff0e447bfa12c65
sotyy <sogolo213@gmail.com>
2017-02-24 16:53:42 +0300

    initial index.html

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//$(document).ready(function(){
+//$(".btn").submit(function(event){
+//event.preventDefault();
+//$("input#datainput").val();
+//});
+//});
+
+$(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+        event.preventDefault();
+        var datainput = parseInt($("input#userinput").val());
+        var numbers = [];
+        for (var i = 1; i <= datainput; i++) {
+            numbers.push(i);
+        }
+        for (var y = 0; y < numbers.length; y++) {
+            if (numbers[y] % 15 === 0) {
+                numbers[y] = "pingpong";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else if (numbers[y] % 5 === 0) {
+                numbers[y] = "pong";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else if (numbers[y] % 3 === 0) {
+                numbers[y] = "ping";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else {
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            }
+        }; // FRONT END LOGIC
+
+    });
+});
