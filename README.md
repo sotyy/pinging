b143c14ff931393d7e2d46afbd8e7decdcf49f70
sotyy <sogolo213@gmail.com>
2017-02-24 18:01:14 +0300

    initial README commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Ping Pong Game
+Specs:
+1. Function returns false when a number is not divisible by 3
+2. Function returns true when a number is divisible by 3
+2. Function returns false when a number is not divisible by 5
+4. Function returns true when a number is divisible by 5
+3. Function returns false when a number is not divisible by 15
+6. Function returns true when a number is  divisible by 15
+7. Refactoring - Funtion returns true if a number is divisible by 3, 5, 15 else return false
+Visit my site on [on GitHub]https://github.com/sotyy/pinging.git .
+## Description
+Ping pong game is a game for one or more player where by you choose a number and it displays a list of number from one to your desired number. But multiple of 3 gives ping, multiples of 5 gives pong and multiples of 15 gives pingpong
--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 
-var toPingPong=function(number){
-  if((number % 3 == 0) && (number % 5 == 0) && (number % 15 == 0)){
-    return true;
-  }else {
-    return false;
-  }
-}
+
 //$(document).ready(function(){
 //$(".btn").submit(function(event){
 //event.preventDefault();
@@ -14,28 +8,28 @@ var toPingPong=function(number){
 //});
 //});
 //
-// $(document).ready(function() {
-//     $("#blanks form").submit(function(event) {
-//         event.preventDefault();
-//         var datainput = parseInt($("input#userinput").val());
-//         var numbers = [];
-//         for (var i = 1; i <= datainput; i++) {
-//             numbers.push(i);
-//         }
-//         for (var y = 0; y < numbers.length; y++) {
-//             if (numbers[y] % 15 === 0) {
-//                 numbers[y] = "pingpong";
-//                 $("#result").append("<li>" + numbers[y] + "</li>");
-//             } else if (numbers[y] % 5 === 0) {
-//                 numbers[y] = "pong";
-//                 $("#result").append("<li>" + numbers[y] + "</li>");
-//             } else if (numbers[y] % 3 === 0) {
-//                 numbers[y] = "ping";
-//                 $("#result").append("<li>" + numbers[y] + "</li>");
-//             } else {
-//                 $("#result").append("<li>" + numbers[y] + "</li>");
-//             }
-//         }; // FRONT END LOGIC
+$(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+        event.preventDefault();
+        var datainput = parseInt($("input#userinput").val());
+        var numbers = [];
+        for (var i = 1; i <= datainput; i++) {
+            numbers.push(i);
+        }
+        for (var y = 0; y < numbers.length; y++) {
+            if (numbers[y] % 15 === 0) {
+                numbers[y] = "pingpong";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else if (numbers[y] % 5 === 0) {
+                numbers[y] = "pong";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else if (numbers[y] % 3 === 0) {
+                numbers[y] = "ping";
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            } else {
+                $("#result").append("<li>" + numbers[y] + "</li>");
+            }
+        }; // FRONT END LOGIC
 
     });
 });
