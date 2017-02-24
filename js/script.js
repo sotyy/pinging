77415c0ae66441b984aa2276df950145dfe188b8
sotyy <sogolo213@gmail.com>
2017-02-24 17:31:31 +0300

    intial true and false on function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
+var toPingPong=function(number){
+  return false;
+}
+
+
+
+var toPingPong=function(number){
+  if(number % 3 == 0){
+    return true;
+  }else{
+    return false;
 //$(document).ready(function(){
 //$(".btn").submit(function(event){
 //event.preventDefault();
 //$("input#datainput").val();
-//});
-//});
 
-$(document).ready(function() {
-    $("#blanks form").submit(function(event) {
-        event.preventDefault();
-        var datainput = parseInt($("input#userinput").val());
-        var numbers = [];
-        for (var i = 1; i <= datainput; i++) {
-            numbers.push(i);
-        }
-        for (var y = 0; y < numbers.length; y++) {
-            if (numbers[y] % 15 === 0) {
-                numbers[y] = "pingpong";
-                $("#result").append("<li>" + numbers[y] + "</li>");
-            } else if (numbers[y] % 5 === 0) {
-                numbers[y] = "pong";
-                $("#result").append("<li>" + numbers[y] + "</li>");
-            } else if (numbers[y] % 3 === 0) {
-                numbers[y] = "ping";
-                $("#result").append("<li>" + numbers[y] + "</li>");
-            } else {
-                $("#result").append("<li>" + numbers[y] + "</li>");
-            }
-        }; // FRONT END LOGIC
+//});
+//});
+//
+// $(document).ready(function() {
+//     $("#blanks form").submit(function(event) {
+//         event.preventDefault();
+//         var datainput = parseInt($("input#userinput").val());
+//         var numbers = [];
+//         for (var i = 1; i <= datainput; i++) {
+//             numbers.push(i);
+//         }
+//         for (var y = 0; y < numbers.length; y++) {
+//             if (numbers[y] % 15 === 0) {
+//                 numbers[y] = "pingpong";
+//                 $("#result").append("<li>" + numbers[y] + "</li>");
+//             } else if (numbers[y] % 5 === 0) {
+//                 numbers[y] = "pong";
+//                 $("#result").append("<li>" + numbers[y] + "</li>");
+//             } else if (numbers[y] % 3 === 0) {
+//                 numbers[y] = "ping";
+//                 $("#result").append("<li>" + numbers[y] + "</li>");
+//             } else {
+//                 $("#result").append("<li>" + numbers[y] + "</li>");
+//             }
+//         }; // FRONT END LOGIC
 
     });
 });
